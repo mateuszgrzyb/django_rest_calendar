335186312fed65d228ddb29f90ab4fc918b26c13
Mateusz Grzyb <grzybmateusz@protonmail.com>
2021-05-12 20:19:16 +0200

    heroku

--- django_rest/serializers.py
+++ django_rest/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models.base import ModelBase
 from rest_framework import serializers
 
 from location.models import Room
@@ -5,20 +6,51 @@ from meeting.models import Event
 from user.models import User
 
 
-class MyModelSerializer(serializers.HyperlinkedModelSerializer):
+def all_subclasses(cls):
+    return set(cls.__subclasses__()).union(
+        s for c in cls.__subclasses__() for s in all_subclasses(c)
+    )
 
-    #
+
+class MyModelSerializerMetaclass(serializers.SerializerMetaclass):
+    def __new__(mcs, name, bases, attrs):
+        Meta = attrs.get('Meta')
+        if Meta is not None and not hasattr(Meta, 'abstract'):
+            assert hasattr(Meta, 'model'), f'{name}.Meta needs field "model"'
+            assert isinstance(Meta.model, ModelBase), \
+                f'{name}.Meta.model should be of type ModelBase'
+
+        return super().__new__(mcs, name, bases, attrs)
+
+
+class MyModelSerializer(
+    serializers.HyperlinkedModelSerializer,
+    metaclass=MyModelSerializerMetaclass
+):
     @property
     def relations(self):
         # circular dependencies
         from location.serializers import RoomSerializer
         from meeting.serializers import EventSerializer
         from user.serializers import UserSerializer
+
+        # FIXME:
+        #   MyModelSerializer.__subclasses__()?
         return {
             Room: RoomSerializer,
             User: UserSerializer,
             Event: EventSerializer,
         }
+
+    # FIXME:
+    #   more general, slower????
+    # @property
+    # def relations(self):
+    #     return {
+    #         cls.Meta.model: cls
+    #         for cls in all_subclasses(MyModelSerializer)
+    #         if hasattr(cls, 'Meta')
+    #     }
 
     def build_nested_field(self, field_name, relation_info, nested_depth):
         _, kwargs = \
@@ -38,5 +70,13 @@ class MyModelSerializer(serializers.HyperlinkedModelSerializer):
             class Meta(field_class.Meta):
                 depth = nested_depth - 1
 
-        return InnerSerializer, kwargs
+        # fix for MyModelSerializer.__subclasses__() hook
+        # 1.
+        # InnerSerializer.__bases__ = (serializers.HyperlinkedModelSerializer,)
+        # 2.
+        # import gc
+        # gc.collect()
 
+        # print(f'no of MMS subclasses: {len(all_subclasses(MyModelSerializer))}')
+
+        return InnerSerializer, kwargs
